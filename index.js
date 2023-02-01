2e79baca9b27d63892f32b58800b1b1c7329a9fd
Vitor Dacio <vitordacio@hotmail.com>
2023-02-01 11:22:33 -0300

    vercelconfigs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const routes = require('./src/routes');
-const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json())
+
 app.use(routes)
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}`))
 
